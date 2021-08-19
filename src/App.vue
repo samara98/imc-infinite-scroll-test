e64b9586954b6ff3e9200efe14520900204a243c
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-08-19 19:14:41 +0700

    add infinite scroll basic

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,238 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <navbar></navbar>
+
+    <v-spacer></v-spacer>
+
+    <v-main>
+      <v-container>
+        <div v-for="photo in photos" :key="photo.id">
+          <img :src="photo.download_url" :alt="photo.author" height="200" />
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue';
+import Navbar from './components/Navbar.vue';
 
-export default Vue.extend({
-  name: "App",
+const photos = [
+  {
+    id: '0',
+    author: 'Alejandro Escamilla',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    download_url: 'https://picsum.photos/id/0/5616/3744',
+  },
+  {
+    id: '1',
+    author: 'Alejandro Escamilla',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/LNRyGwIJr5c',
+    download_url: 'https://picsum.photos/id/1/5616/3744',
+  },
+  {
+    id: '10',
+    author: 'Paul Jarvis',
+    width: 2500,
+    height: 1667,
+    url: 'https://unsplash.com/photos/6J--NXulQCs',
+    download_url: 'https://picsum.photos/id/10/2500/1667',
+  },
+  {
+    id: '100',
+    author: 'Tina Rataj',
+    width: 2500,
+    height: 1656,
+    url: 'https://unsplash.com/photos/pwaaqfoMibI',
+    download_url: 'https://picsum.photos/id/100/2500/1656',
+  },
+  {
+    id: '1000',
+    author: 'Lukas Budimaier',
+    width: 5626,
+    height: 3635,
+    url: 'https://unsplash.com/photos/6cY-FvMlmkQ',
+    download_url: 'https://picsum.photos/id/1000/5626/3635',
+  },
+  {
+    id: '1001',
+    author: 'Danielle MacInnes',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/1DkWWN1dr-s',
+    download_url: 'https://picsum.photos/id/1001/5616/3744',
+  },
+  {
+    id: '1002',
+    author: 'NASA',
+    width: 4312,
+    height: 2868,
+    url: 'https://unsplash.com/photos/6-jTZysYY_U',
+    download_url: 'https://picsum.photos/id/1002/4312/2868',
+  },
+  {
+    id: '1003',
+    author: 'E+N Photographies',
+    width: 1181,
+    height: 1772,
+    url: 'https://unsplash.com/photos/GYumuBnTqKc',
+    download_url: 'https://picsum.photos/id/1003/1181/1772',
+  },
+  {
+    id: '1004',
+    author: 'Greg Rakozy',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/SSxIGsySh8o',
+    download_url: 'https://picsum.photos/id/1004/5616/3744',
+  },
+  {
+    id: '1005',
+    author: 'Matthew Wiebe',
+    width: 5760,
+    height: 3840,
+    url: 'https://unsplash.com/photos/tBtuxtLvAZs',
+    download_url: 'https://picsum.photos/id/1005/5760/3840',
+  },
+  {
+    id: '1006',
+    author: 'Vladimir Kudinov',
+    width: 3000,
+    height: 2000,
+    url: 'https://unsplash.com/photos/-wWRHIUklxM',
+    download_url: 'https://picsum.photos/id/1006/3000/2000',
+  },
+  {
+    id: '1008',
+    author: 'Benjamin Combs',
+    width: 5616,
+    height: 3744,
+    url: 'https://unsplash.com/photos/5L4XAgMSno0',
+    download_url: 'https://picsum.photos/id/1008/5616/3744',
+  },
+  {
+    id: '1009',
+    author: 'Christopher Campbell',
+    width: 5000,
+    height: 7502,
+    url: 'https://unsplash.com/photos/CMWRIzyMKZk',
+    download_url: 'https://picsum.photos/id/1009/5000/7502',
+  },
+  {
+    id: '101',
+    author: 'Christian Bardenhorst',
+    width: 2621,
+    height: 1747,
+    url: 'https://unsplash.com/photos/8lMhzUjD1Wk',
+    download_url: 'https://picsum.photos/id/101/2621/1747',
+  },
+  {
+    id: '1010',
+    author: 'Samantha Sophia',
+    width: 5184,
+    height: 3456,
+    url: 'https://unsplash.com/photos/NaWKMlp3tVs',
+    download_url: 'https://picsum.photos/id/1010/5184/3456',
+  },
+  {
+    id: '1011',
+    author: 'Roberto Nickson',
+    width: 5472,
+    height: 3648,
+    url: 'https://unsplash.com/photos/7BjmDICVloE',
+    download_url: 'https://picsum.photos/id/1011/5472/3648',
+  },
+  {
+    id: '1012',
+    author: 'Scott Webb',
+    width: 3973,
+    height: 2639,
+    url: 'https://unsplash.com/photos/uAgLGG1WBd4',
+    download_url: 'https://picsum.photos/id/1012/3973/2639',
+  },
+  {
+    id: '1013',
+    author: 'Cayton Heath',
+    width: 4256,
+    height: 2832,
+    url: 'https://unsplash.com/photos/D8LcRLwZyPs',
+    download_url: 'https://picsum.photos/id/1013/4256/2832',
+  },
+  {
+    id: '1014',
+    author: 'Oscar Keys',
+    width: 6016,
+    height: 4000,
+    url: 'https://unsplash.com/photos/AmPRUnRb6N0',
+    download_url: 'https://picsum.photos/id/1014/6016/4000',
+  },
+  {
+    id: '1015',
+    author: 'Alexey Topolyanskiy',
+    width: 6000,
+    height: 4000,
+    url: 'https://unsplash.com/photos/-oWyJoSqBRM',
+    download_url: 'https://picsum.photos/id/1015/6000/4000',
+  },
+];
+
+interface Data {
+  photos: {
+    id: string;
+    author: string;
+    width: number;
+    height: number;
+    url: string;
+    download_url: string;
+  }[];
+  page: number;
+}
+
+interface Methods {
+  handleScroll: () => void;
+}
+
+const App = Vue.extend<Data, Methods, unknown>({
+  name: 'App',
+
   components: {
-    HelloWorld,
+    Navbar,
+  },
+
+  created() {
+    this.photos = photos.slice(0, 5);
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  data: () => ({
+    photos: [],
+    page: 1,
+  }),
+
+  methods: {
+    handleScroll() {
+      let bottomOfWindow =
+        document.documentElement.scrollTop + window.innerHeight ===
+        document.documentElement.offsetHeight;
+      if (bottomOfWindow) {
+        const append = photos.slice(this.photos.length, this.photos.length + 5);
+        this.photos = this.photos.concat(append);
+      }
+    },
   },
 });
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default App;
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   lintOnSave: false,
+
+  transpileDependencies: ['vuetify'],
+
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'IMC Infinite Scroll Test';
+      return args;
+    });
+  },
 };
